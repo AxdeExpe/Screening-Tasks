8a8b718bad6859fe8592c0494e688cbbf392e74c
Kev <103242766+AxdeExpe@users.noreply.github.com>
2025-09-19 23:02:00 +0200

    bugfix

--- Bitcoin price change/solution.py
+++ Bitcoin price change/solution.py
@@ -3,6 +3,7 @@ import re
 import pandas as pd
 from datetime import datetime
 from typing import Iterator, Tuple
+import math 
 
 class OHLCDataReader:
     def __init__(self, path: str = "btc-1h.csv"):
@@ -29,10 +30,7 @@ class OHLCDataReader:
     
     def _is_unix_time(self, timestamp: int) -> bool:
 
-        if (timestamp.bit_length() + 2) // 3 != 13:
-            return False
-        
-        return True
+        return 1e11 <= timestamp <= 2e13
 
     def get_time_diff(self, time1: int, time2: int) -> int:
 
@@ -81,13 +79,13 @@ class OHLCAnalyzer:
                 ohlc_pair[i] = value
                 break
 
-    def analyze_close_change(self, procent_diff: float = 3.0, time_interval: int = 60 * 60 * 1000) -> Iterator[Tuple[float, list]]: # 1 hour
+    def analyze_close_change(self, percent_diff: float = 3.0, time_interval: int = 60 * 60 * 1000) -> Iterator[Tuple[float, list]]: # 1 hour
 
         if self.close_price_name is None or self.close_time_name is None:
             raise ValueError("Close price name or close time name is not set")
 
-        if procent_diff < 0.0 or procent_diff > 100.0:
-            raise ValueError("Procent diff must be between 0 and 100")
+        if percent_diff < 0.0 or percent_diff > 100.0:
+            raise ValueError("percent diff must be between 0 and 100")
 
         header = self.ohlc_reader.get_header()
         close_time_index = header.index(self.close_time_name)
@@ -105,10 +103,11 @@ class OHLCAnalyzer:
                 if time_diff != time_interval:
                     raise ValueError("Time diff is not equal to time_interval")
 
-                price_change_procent = (ohlc_pair[0][close_price_index] - ohlc_pair[1][close_price_index]) / ohlc_pair[0][close_price_index] * 100
+                price_change_percent = (ohlc_pair[0][close_price_index] - ohlc_pair[1][close_price_index]) / ohlc_pair[0][close_price_index] * 100
+                price_change_percent_abs = abs(price_change_percent)
 
-                if price_change_procent > procent_diff:
-                    yield price_change_procent, ohlc_pair
+                if price_change_percent_abs > percent_diff:
+                    yield price_change_percent_abs, ohlc_pair
 
                 ohlc_pair[0] = ohlc_pair[1]
                 ohlc_pair[1] = None
@@ -122,14 +121,14 @@ class OHLCAnalyzer:
         close_time_index = header.index(self.close_time_name)
         close_price_index = header.index(self.close_price_name)
 
-        for i, (price_change_procent, ohlc_pair) in enumerate(self.analyze_close_change(procent_diff=3.0, time_interval=60 * 60 * 1000)):
+        for i, (price_change_percent, ohlc_pair) in enumerate(self.analyze_close_change(percent_diff=3.0, time_interval=60 * 60 * 1000)):
             
             if i >= n:
                 break
 
             close_time_start = self.convert_unix_to_datetime(int(ohlc_pair[0][close_time_index]))
             close_time_end = self.convert_unix_to_datetime(int(ohlc_pair[1][close_time_index]))
-            print("{:<10.2f}% {:<10} {:<10} {:<10} {:<10}".format(price_change_procent, close_time_start, ohlc_pair[0][close_price_index], close_time_end, ohlc_pair[1][close_price_index]))
+            print("{:<10.2f}% {:<10} {:<10} {:<10} {:<10}".format(price_change_percent, close_time_start, ohlc_pair[0][close_price_index], close_time_end, ohlc_pair[1][close_price_index]))
 
 
 
